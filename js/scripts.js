37da86fd675256a4b282e8a98ebd4c41139a334a
lilbitsnpieces <alreadywrote@gmail.com>
2023-01-17 19:34:45 -0800

    Add -,*, and /.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,18 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function subtract(number1, number2) {
+    return number1 / number2;
+}
+
 //user interface logic 
 const number1 = parseInt(prompt("Enter a number: "));
 const number2 = parseInt(prompt("Enter another number: "));
