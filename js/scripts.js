25f7a843fba40643fc93cbd5459093fa8f9322e4
lilbitsnpieces <alreadywrote@gmail.com>
2023-01-17 21:32:49 -0800

    Continue to troubleshoot adding input to alert.

--- js/scripts.js
+++ js/scripts.js
@@ -20,10 +20,9 @@ const number1 = parseInt(prompt("Enter a number: "));
 const number2 = parseInt(prompt("Enter another number: "));
 
 
-
 const addition = window.alert("The addition of the numbers equals: " + add(number1, number2) + 
 " The subtraction of the numbers equals: " + subtract(number1, number2) 
-+ "  The multiplication of the numbers equals: " + multiply(number1, number2) + 
++ " The multiplication of the numbers equals: " + multiply(number1, number2) + 
 " The division of the numbers equals: " + divide(number1, number2)); 
 
 //window.alert("The subtraction of the numbers equals: " + subtract(number1, number2));
