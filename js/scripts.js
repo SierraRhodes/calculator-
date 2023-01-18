b8dad3a0978186f3c20121aa410a7d6e614ff556
lilbitsnpieces <alreadywrote@gmail.com>
2023-01-17 19:54:00 -0800

    Fixed typo in subtract alert.

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ function multiply(number1, number2) {
   return number1 * number2;
 }
 
-function subtract(number1, number2) {
+function divide(number1, number2) {
     return number1 / number2;
 }
 
@@ -20,3 +20,7 @@ const number1 = parseInt(prompt("Enter a number: "));
 const number2 = parseInt(prompt("Enter another number: "));
 
 window.alert(add(number1, number2));
+window.alert(subtract(number1, number2));
+window.alert(multiply(number1, number2));
+window.alert(divide(number1, number2));
+
